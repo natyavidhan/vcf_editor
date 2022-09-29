5c656bd7b1b5a4cf2ff6123bc689f3edee16709b
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-09-29 20:32:44 +0530

    lots of functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import filedialog
 
 class App:
     def __init__(self, root):
@@ -21,15 +21,17 @@ class App:
         self.csv_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="CSV", menu=self.csv_menu)
         self.csv_menu.add_command(label="Open CSV", command=self.open_csv)
-        self.csv_menu.add_command(label="Save CSV", command=self.open_csv)
+        self.csv_menu.add_command(label="Save CSV", command=self.save_csv)
 
         self.contact_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Contact", menu=self.contact_menu)
         self.contact_menu.add_command(label="Add Contact", command=self.add_contact)
+        self.contact_menu.add_command(label="Save Contact", command=self.save_contact)
         self.contact_menu.add_command(label="Delete Contact", command=self.delete_contact)
 
         self.contact_list = tk.Listbox(self.root, width=40, height=10)
         self.contact_list.place(x=10, y=15, width=90, height=165)
+        self.contact_list.bind("<<ListboxSelect>>", self.select_contact)
 
         tk.Label(self.root, text="Name", anchor="e").place(x=115, y=15, width=60, height=15)
         self.name = tk.Entry(self.root)
@@ -50,6 +52,8 @@ class App:
         tk.Label(self.root, text="Address", anchor="e").place(x=115, y=115, width=60, height=15)
         self.address = tk.Text(self.root)
         self.address.place(x=190, y=115, width=100, height=55)
+
+        self.contacts = []
     
     def parse_vcf(self, contact:str):
         contact = contact.split("\n")
@@ -69,26 +73,118 @@ class App:
     
     def gen_vcf(self, contact:dict):
         contact['address'] = contact['address'].replace("\n", "\\n")
-        c = f"BEGIN:VCARD\nVERSION:3.0\nN:{contact['name']}\nFN:{contact['name']}\nTITLE:{contact['nickname']}\nTEL;TYPE=CELL:{contact['phone']}\nEMAIL:{contact['email']}\nADR;TYPE#WORK,PREF:;;{contact['address']}\nEND:VCARD"
+        c = f"BEGIN:VCARD\nVERSION:3.0\nN:{contact['name']}\nFN:{contact['name']}\nTITLE:{contact['nickname']}\nTEL;TYPE=CELL:{contact['phone']}\nEMAIL:{contact['email']}\nADR;TYPE#WORK,PREF:;;{contact['address']}\nEND:VCARD\n"
         return c
 
     def open_vcf(self):
-        pass
+        file = filedialog.askopenfilename(filetypes=[("VCF Files", "*.vcf")])
+        if file:
+            with open(file, "r") as f:
+                contacts = f.read().split("BEGIN:VCARD")
+            self.contacts = []
+            for contact in contacts:
+                if contact:
+                    self.contacts.append(self.parse_vcf(contact))
+            self.load_contacts()
 
     def save_vcf(self):
-        pass
+        file = filedialog.asksaveasfilename(filetypes=[("VCF Files", "*.vcf")])
+        if file:
+            with open(file, "w") as f:
+                for contact in self.contacts:
+                    f.write(self.gen_vcf(contact))
 
     def open_csv(self):
-        pass
+        file = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+        if file:
+            with open(file, "r") as f:
+                contacts = f.read().split("\n")
+            self.contacts = []
+            for contact in contacts:
+                if contact:
+                    try:
+                        name, phone, nickname, email, address = contact.split(",")
+                        self.contacts.append({"name":name, "nickname":nickname, "phone":phone, "email":email, "address":address})
+                    except:
+                        try:
+                            name, phone, nickname, email = contact.split(",")
+                            self.contacts.append({"name":name, "nickname":nickname, "phone":phone, "email":email})
+                        except:
+                            try:
+                                name, phone, nickname = contact.split(",")
+                                self.contacts.append({"name":name, "nickname":nickname, "phone":phone})
+                            except:
+                                try:
+                                    name, phone = contact.split(",")
+                                    self.contacts.append({"name":name, "phone":phone})
+                                except:
+                                    pass
+            self.load_contacts()
 
     def save_csv(self):
-        pass
+        file = filedialog.asksaveasfilename(filetypes=[("CSV Files", "*.csv")])
+        if file:
+            with open(file, "w") as f:
+                for contact in self.contacts:
+                    s = ""
+                    s+=contact['name']+","
+                    s+=contact['phone']+","
+                    try:
+                        s+=contact['nickname']+","
+                    except:
+                        s+=","
+                    try:
+                        s+=contact['email']+","
+                    except:
+                        s+=","
+                    try:
+                        s+=contact['address']
+                    except:
+                        pass
+                    f.write(s+"\n")
 
     def add_contact(self):
-        pass
+        self.contacts.append({"name":"","nickname":"","phone":"","email":"","address":""})
+        self.load_contacts()
+    
+    def save_contact(self):
+        index = self.contact_list.curselection()[0]
+        self.contacts[index]["name"] = self.name.get()
+        self.contacts[index]["nickname"] = self.nickname.get()
+        self.contacts[index]["phone"] = self.phone.get()
+        self.contacts[index]["email"] = self.email.get()
+        self.contacts[index]["address"] = self.address.get("1.0", "end-1c")
+    
+    def select_contact(self, event):
+        index = self.contact_list.curselection()[0]
+        self.name.delete(0, "end")
+        self.name.insert(0, self.contacts[index]["name"])
+        try:
+            self.nickname.delete(0, "end")
+            self.nickname.insert(0, self.contacts[index]["nickname"])
+        except:
+            pass
+        self.phone.delete(0, "end")
+        self.phone.insert(0, self.contacts[index]["phone"])
+        try:
+            self.email.delete(0, "end")
+            self.email.insert(0, self.contacts[index]["email"])
+        except:
+            pass
+        try:
+            self.address.delete("1.0", "end")
+            self.address.insert("1.0", self.contacts[index]["address"])
+        except:
+            pass
 
     def delete_contact(self):
-        pass
+        self.contacts.pop(self.contact_list.curselection()[0])
+        self.load_contacts()
+
+    def load_contacts(self):
+        self.contact_list.delete(0, tk.END)
+        for contact in self.contacts:
+            self.contact_list.insert(tk.END, contact["name"])
 
 if __name__ == "__main__":
     root = tk.Tk()
